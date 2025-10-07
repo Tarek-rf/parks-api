13ca4fb8143933b174170b2a15e5256fcebb09de
Matthew-Veroutis <veroutism@gmail.com>
2025-10-07 11:15:30 -0400

    Added history controller and model
    Added history controller and model

--- app/Domain/Models/HistoryModel.php
+++ app/Domain/Models/HistoryModel.php
@@ -4,6 +4,9 @@ namespace App\Domain\Models;
 
 use App\Helpers\Core\PDOService;
 
+/**
+ * model use to fetch history data from the database 
+ */
 class HistoryModel extends BaseModel
 {
     /**
@@ -15,5 +18,38 @@ class HistoryModel extends BaseModel
         parent::__construct($pdo);
     }
 
-    
+    /**
+     * fetches a list of history based on the filters from the database
+     * @param array $filters the filters
+     * @return mixed the list of history
+     */
+    function getHistory(array $filters): mixed
+    {
+        $pdo_values = [];
+
+        $query = "SELECT * FROM history WHERE 1  ";
+
+        if(isset($filters["country"]) && !empty($filters["country"])){
+            $query.= " AND country LIKE CONCAT('%',:name_of_country,'%')";
+            $pdo_values["name_of_country"] = $filters["country"];
+        }
+
+        if(isset($filters["founded_date_after"]) && !empty($filters["founded_date_after"])){
+            $query.= " AND founded_date > :founded_after";
+            $pdo_values["founded_after"] = $filters["founded_date_after"];
+        }
+
+        if(isset($filters["founded_date_before"]) && !empty($filters["founded_date_before"])){
+            $query.= " AND founded_date < :founded_before";
+            $pdo_values["founded_before"] = $filters["founded_date_before"];
+        }
+
+        $history = $this->paginate(
+            $query,
+            $pdo_values
+        );
+
+        return $history;
+    }
+
 }
