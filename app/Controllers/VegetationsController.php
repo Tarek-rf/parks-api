11da56f69fb803e3d1facdc34b95ee25090ea030
mjk-22 <6231001@edu.vaniercollege.qc.ca>
2025-10-08 12:53:22 -0400

    corrected the read function of vegetation
    now you can access vegetation from thunder client. i did a bit of the specs

--- app/Controllers/VegetationsController.php
+++ app/Controllers/VegetationsController.php
@@ -8,13 +8,13 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class VegetationsController extends BaseController
 {
-     private VegetationsModel $vegetations_model;
+    //  private VegetationsModel $vegetations_model;
 
     /**
      * Creates an object of vegetations controller
      * @param $vegetations_model the model of the vegetations class later used to fetch from the database
      */
-    public function __construct(VegetationsModel $vegetations_model)
+    public function __construct(private VegetationsModel $vegetations_model)
     {
         // $this->vegetations_model = $vegetations_model;
     }
@@ -23,21 +23,21 @@ class VegetationsController extends BaseController
       * Get a collection resource of vegetations
       * @param $request the request sent by the client
       * @param  $response the response sent by the server to the client
-      * @return Response the response sent to the the client 
+      * @return Response the response sent to the the client
       */
      public function handleGetVegetations(Request $request, Response $response): Response
     {
         $filters = $request->getQueryParams();
-        //TODO merge with mathew to get the pagination method
+
         //Todo validation of pagination params
-        // $this->setPaginationParams($filters, $this->vegetations_model);
+        $this->setPaginationParams($filters, $this->vegetations_model,$request);
 
         //Todo validate the filters
         //TODO errors
 
-        $animals = $this->vegetations_model->getVegetations($filters);
+        $vegetations = $this->vegetations_model->getVegetations($filters);
 
-        return $this->renderJson($response, $animals);
+        return $this->renderJson($response, $vegetations);
     }
 
     //TODO implement handleGetVegetationsById
